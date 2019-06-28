44a087b3f480beb2ad3d0944b81fd602e3a6ed95
jeremyjouffroy <jeremy.jouffroy@gmail.com>
2019-06-28 17:14:36 +0200

    adding comments in Application controler to describe the different authentication and authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < ActionController::Base
-  include Pundit
+  # Protect applications from CSRF vulnerability.
   protect_from_forgery with: :exception
+  # all the application views require that the user be logged in except
+  # indexes and/or decided otherwiser in the controllers
   before_action :authenticate_user!, :except => [:index]
+  # Manage/tune access right / policies with Pundit Gem
+  include Pundit
+
+  # Pundit: white-list approach.
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # def user_not_authorized
+  #   flash[:alert] = "You are not authorized to perform this action."
+  #   redirect_to(root_path)
+  # end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
